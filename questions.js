410ca5b249f35e4e946ef9b8c23119e6acbad4d4
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-02-09 11:24:34 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,28 @@
+const questions = {
+    'anime': [
+        {
+            question: 'Who is your favorite anime character?',
+            options: ['Naruto Uzumaki', 'Monkey D. Luffy', 'Goku', 'Saitama'],
+            answer: 'Naruto Uzumaki'
+        },
+        {
+            question: 'Which anime is set in the Sengoku period of Japan?',
+            options: ['Naruto', 'One Piece', 'Inuyasha', 'Bleach'],
+            answer: 'Inuyasha'
+        }
+        // Add more anime questions here
+    ],
+    'movies': [
+        {
+            question: 'What is your favorite movie genre?',
+            options: ['Action', 'Comedy', 'Horror', 'Romance'],
+            answer: 'Action'
+        },
+        {
+            question: 'Who directed the movie "Inception"?',
+            options: ['Christopher Nolan', 'Steven Spielberg', 'James Cameron', 'Martin Scorsese'],
+            answer: 'Christopher Nolan'
+        }
+        // Add more movie questions here
+    ]
+};
